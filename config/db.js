7801291f7ce5f60cd2bd0121dce4d18196fb0def
HARSH <103347264+ha-rshh@users.noreply.github.com>
2023-02-10 00:01:14 +0530

    done with frontend completed

---
+++ config/db.js
@@ -0,0 +1,22 @@
+
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+function connectDB() {
+      mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+      });
+
+      const connection = mongoose.connection;
+
+      connection.once('open', () => {
+            console.log("DB connected successfully");
+      });
+      connection.on('error', () => {
+            console.log("DB connection failed");
+      });
+}
+
+module.exports = connectDB;
+// mIAY0a6u1ByJsWWZ
