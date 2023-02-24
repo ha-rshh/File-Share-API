d9c99aa12d8bb88ed6319061abfe2e4098eba2ba
HARSH <103347264+ha-rshh@users.noreply.github.com>
2023-02-24 17:02:55 +0530

    Update files.js

--- routes/files.js
+++ routes/files.js
@@ -1,35 +1,41 @@
-const router = require('express').Router();
-const multer = require('multer');
-const path = require('path');
-const File = require('../models/file');
-const { v4: uuidv4 } = require('uuid');
+const router = require("express").Router();
+const multer = require("multer");
+const path = require("path");
+const File = require("../models/file");
 
 let storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/') ,
-    filename: (req, file, cb) => {
-        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
-              cb(null, uniqueName)
-    } ,
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => {
+    const uniqueName = `${Date.now()}-${Math.round(
+      Math.random() * 1e9
+    )}${path.extname(file.originalname)}`;
+    cb(null, uniqueName);
+  },
 });
 
-let upload = multer({ storage, limits:{ fileSize: 1000000 * 100 }, }).single('myfile'); //100mb
+let upload = multer({ storage }).single("myfile");
 
-router.post('/', (req, res) => {
-    upload(req, res, async (err) => {
-      if (err) {
-        return res.status(500).send({ error: err.message });
-      }
-      console.log(req)
-        const file = new File({
-            filename: req.file.filename,
-            uuid: uuidv4(),
-            path: req.file.path,
-            size: req.file.size
-        });
-        const response = await file.save();
-        res.json({ file: `${process.env.APP_BASE_URL}/files/${response.uuid}` });
-      });
+router.post("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  upload(req, res, async (err) => {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+    } else if (err) {
+      // An unknown error occurred when uploading.
+    }
+    const file = new File({
+      filename: req.file.filename,
+      size: req.file.size,
+      path: req.file.path,
+      size: req.file.size,
+    });
+    const response = await file.save();
+    // Everything went fine.
+    res.json({ file: `${process.env.APP_BASE_URL}/files/${response._id}` });
+  });
 });
+
+
 
 router.post('/send', async (req, res) => {
   const { uuid, emailTo, emailFrom, expiresIn } = req.body;
